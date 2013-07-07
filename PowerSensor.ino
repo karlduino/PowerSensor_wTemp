e41206d475a454241e12e97a82387a74bfc80e27
Karlduino <karlduino@gmail.com>
2013-07-07 00:12:56 -0500

    Have it working with ethernet shield to send email

--- PowerSensor.ino
+++ PowerSensor.ino
@@ -1,30 +1,102 @@
 /**
  PowerSensor
  
- Sensor power on pin 2
- Power on: light led on pin 13 (green)
- Power off: light led on pin 12 (red)
+ Sense power on pin 2
+ Power on: light led on pin 8 (green)
+ Power off: light led on pin 7 (red)
+
+ If power goes off, send an email (using telnet to yahoo account)
+ Following this post from Rob Glinka: http://bit.ly/12PivcA
 **/
 
+#include <SPI.h>
+#include <Ethernet.h>
+
 static int inputPin = 2;
-static int redPin = 12;
-static int greenPin = 13;
+static int redPin = 7;
+static int greenPin = 8;
+
+// private info [made generic here]
+byte mac[] = { 0x00, 0xC1, 0x4E, 0xF2, 0xDA, 0x30 }; // local MAC
+char email_auth[] = "encoded_email_and_password";
+char from_email[] = "<email_addr@yahoo.com>";
+char to_email[] = "<another_email@gmail.com>";
+// end of private info
+
+EthernetClient client;
+byte server[] = { 63, 250, 193, 228 }; // Mail server address  (smtp.mail.yahoo.com)
+
+unsigned long lastTimeSent, currentTime;
+static unsigned long halfhour = 1800000;
+int numberSent = 0;
 
 void setup() {
   pinMode(inputPin, INPUT);
   pinMode(redPin, OUTPUT);
   pinMode(greenPin, OUTPUT);  
+
+  Ethernet.begin(mac);
+  Serial.begin(9600);
+
+  lastTimeSent = millis();
+  Serial.println("Ready.");
 }
 
 void loop() {
  if(digitalRead(inputPin) == HIGH) {
    digitalWrite(greenPin, HIGH);
    digitalWrite(redPin, LOW);
+   numberSent = 0;
  }
  else {
+   currentTime = millis();
    digitalWrite(greenPin, LOW);
    digitalWrite(redPin, HIGH);
+  
+   if(numberSent==0 || currentTime > lastTimeSent + halfhour) {
+     numberSent++;
+     lastTimeSent = millis();
+     Serial.print("Sending email. ");
+     Serial.println(numberSent);
+     send_email();
+   }
+
  }
- delay(500);
 }
 
+
+void send_email()
+{
+  Serial.println("Connecting...");
+  if(client.connect(server, 25)) {
+ 
+    Serial.println("Connected.");
+    delay(1000);
+    Serial.println("Sending...");
+
+    client.println("HELO smpt.mail.yahoo.com");
+    delay(50);
+    client.print("AUTH PLAIN ");
+    client.println(email_auth);
+    delay(50);
+    client.print("mail from:");
+    client.println(from_email);
+    client.print("rcpt to:");
+    client.println(to_email);
+    delay(50);
+    client.println("data");
+    client.print("The sump pump is down. (");
+    client.print(numberSent);
+    client.println(")");
+    client.println(".");
+    delay(50);
+
+    Serial.println("Disconnecting...");
+    client.println("quit");
+    delay(1000);
+    Serial.println("Done.");
+    client.stop();
+  } else {
+    Serial.println("Connection failed.");
+  }
+}
---
+++ ReadMe.md
@@ -0,0 +1,17 @@
+### PowerSensor
+
+The aim of this project is to sense my sump pump losing power and send
+a email/text.
+
+- I connected a 5V wall adapter to pin 2, with a 10k pull-down
+  resistor
+- A green LED is connected to pin 8, lit when power is on.
+- A red LED is connected to pin 7, lit when power is off.
+- Using an ethernet shield, send an email if the power on pin 2 goes
+  off.  I'm using telnet with a yahoo account, following
+  [this post](http://bit.ly/12PivcA) from Rob Glinka.
+
+Of course, if my web connection goes down (my cable modem or router),
+this won't work.
+
+       
